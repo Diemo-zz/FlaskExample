08113c234fdbe91d57b35e4de219e69518027e01
Diarmaid <diarmaiddeburca@gmail.com>
2019-01-05 13:59:26 +0000

    Adding in endpoints to use

---
+++ Endpoints/Time.py
@@ -0,0 +1,4 @@
+from flask_restful import Resource
+class Time(Resource):
+    def get(self):
+        pass
--- app.py
+++ app.py
@@ -1,11 +1,19 @@
 from flask import Flask
-from flask_restful import Resource, Api
+from flask_restful import Api
+from Endpoints.Initial import Initial
+from Endpoints.Builings import Buildings
 from importer import Importer
 app = Flask(__name__)
 
 api = Api(app)
 
-api.add_resource(Importer, '/')
+api.add_resource(Initial, '/')
+
+api.add_resource(Importer, '/importer')
+
+api.add_resource(Buildings, '/buildings')
+
+api.add_resource(Time, '/time')
 
 if __name__ == "__main__":
     app.run()
